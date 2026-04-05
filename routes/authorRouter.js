cb6f7bfc755211dd8c0b70b4cf0cd3b814106e11
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-04-05 16:21:54 +0300

    Write author related routes

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,9 +1,41 @@
 const { Router } = require("express");
-const { requireAuth, requireAuthor } = require("../middleware/auth");
-const { getAllArticles } = require("../controllers/authorController");
+const {
+  requireAuth,
+  requireAuthor,
+  requireOwner,
+} = require("../middleware/auth");
+const {
+  getAllArticles,
+  postArticle,
+  deleteArticle,
+  updateArticle,
+} = require("../controllers/authorController");
+const { findArticleById } = require("../services/articleServices");
 
 const authorRouter = Router();
 
 authorRouter.get("/articles", requireAuth, requireAuthor, getAllArticles);
-
+authorRouter.post("/articles", requireAuth, requireAuthor, postArticle);
+authorRouter.delete(
+  "/articles/:articleId",
+  requireAuth,
+  requireAuthor,
+  requireOwner({
+    idParam: "articleId",
+    findById: findArticleById,
+    ownerField: "userId",
+  }),
+  deleteArticle,
+);
+authorRouter.patch(
+  "/articles/:articleId",
+  requireAuth,
+  requireAuthor,
+  requireOwner({
+    idParam: "articleId",
+    findById: findArticleById,
+    ownerField: "userId",
+  }),
+  updateArticle,
+);
 module.exports = authorRouter;
