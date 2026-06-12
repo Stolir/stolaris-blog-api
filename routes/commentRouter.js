41ee510d3b64d940bf585629f042fce92e98bc63
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-12 14:56:04 +0300

    Add route to get all comments

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { requireOwner, requireAuth } = require("../middleware/auth");
 const { deleteComment } = require("../controllers/articleController");
 const { findCommentById } = require("../services/commentServices");
+const { getAllComments } = require("../controllers/commentsController");
 
 const commentRouter = Router();
 const ownerCheck = requireOwner({
@@ -9,6 +10,8 @@ const ownerCheck = requireOwner({
   findById: findCommentById,
   ownerField: "userId",
 });
+
+commentRouter.get("/", getAllComments);
 
 commentRouter.delete("/:commentId", requireAuth, ownerCheck, deleteComment);
 
