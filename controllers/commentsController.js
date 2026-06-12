2d57a6ef71c12e423452329768cb32243d73c730
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-12 15:04:56 +0300

    Fix missing express middleware parameters

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,6 +1,6 @@
 const { findAllComments } = require("../services/commentServices");
 
-const getAllComments = async () => {
+const getAllComments = async (req, res, next) => {
   try {
     const comments = await findAllComments();
     res.json(comments);
