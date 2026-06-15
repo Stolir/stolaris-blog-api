437e054df01093395c22f2996bb27254082e347f
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-15 18:59:51 +0300

    Update cors options to accept two origins

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 // Use general middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.USER_CLIENT_URL, process.env.AUTHOR_CLIENT_URL],
     credentials: true,
   }),
 );
